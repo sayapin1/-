f869e20470a1def21dac6a82a86628c2d4bec02e
Joo HyeonJin <icepri3535@gmail.com>
2023-02-01 20:17:42 +0900

    시퀄라이즈, 디렉구조 완성

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
