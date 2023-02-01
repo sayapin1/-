3e62f62b67a9943ce74663e210897718f4472433
sayapin1 <110008118+sayapin1@users.noreply.github.com>
2023-02-01 19:06:52 +0900

    Update empty.js

--- public/images/empty.js
+++ public/images/empty.js
@@ -1,11 +1,1 @@
-await queryInterface.addConstraint("Tokens", {	// FK를 설정할 테이블 (DB에 있는 테이블 이름과 같아야한다.)
-      fields: ["userId"],	// FK로 등록할 필드 이름
-      type: "foreign key",
-      name: "users_tokens_id_fk",
-      references: {
-        table: "Users",		// 참조할 테이블 (DB에 있는 테이블 이름과 같아야한다.)
-        field: "userID",	// 참조할 필드 이름
-      },
-      onDelete: "cascade",
-      onUpdate: "cascade",
-    });
+
